80ad55aa7b37f07b6ad1e01f7f79f65043f1d709
Karl Pichotta <karlpichotta@gmail.com>
2011-04-14 03:30:38 -0500

    working basic implementation

--- search/bingsearch.py
+++ search/bingsearch.py
@@ -1,8 +1,30 @@
-from pybing import Bing
+from pybing.query import WebQuery
+import pprint
+import sys
+import time
 
+class BingSearch:
+
+    __APPID = '335CBE48CCCAF4A34652A3DDE7D2CE78FD3390DC'
+
+    def __init__(self):
+        pass
+
+    def get_contexts(self, query):
+        results = WebQuery(self.__APPID,
+                           query=query).set_offset(0).set_count(50).execute()
+        res = []
+        for r in results:
+            try:
+                res.append(r.description)
+            except AttributeError:
+                pass
+        return res
 
 if __name__ == '__main__':
-    bing = Bing('335CBE48CCCAF4A34652A3DDE7D2CE78FD3390DC')
-    response = bing.search_web('python bing')
-    results = response['SearchResponse']['Web']['Results']
-    print len(results)
+    print "Searching...."
+    t = time.time()
+    li = BingSearch().get_contexts('coffee')
+    print '%d contexts returned.' % len(li)
+    #pprint.pprint(li)
+    print 'Took %d seconds.' % (time.time() - t)
