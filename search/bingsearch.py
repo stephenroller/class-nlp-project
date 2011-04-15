16068062ae025ff18cb19dd62b95091b3b3efb88
Karl Pichotta <karlpichotta@gmail.com>
2011-04-14 19:45:54 -0500

    cleaning up

--- search/bingsearch.py
+++ search/bingsearch.py
@@ -3,28 +3,24 @@ import pprint
 import sys
 import time
 
-class BingSearch:
+APPID = '335CBE48CCCAF4A34652A3DDE7D2CE78FD3390DC'
 
-    __APPID = '335CBE48CCCAF4A34652A3DDE7D2CE78FD3390DC'
 
-    def __init__(self):
-        pass
-
-    def get_contexts(self, query):
-        results = WebQuery(self.__APPID,
-                           query=query).set_offset(0).set_count(50).execute()
-        res = []
-        for r in results:
-            try:
-                res.append(r.description)
-            except AttributeError:
-                pass
-        return res
+def get_contexts(query):
+    results = WebQuery(APPID,
+                       query=query).set_offset(0).set_count(50).execute()
+    res = []
+    for r in results:
+        try:
+            res.append(r.description)
+        except AttributeError:
+            pass
+    return res
 
 if __name__ == '__main__':
     print "Searching...."
     t = time.time()
-    li = BingSearch().get_contexts('coffee')
+    li = get_contexts('coffee')
     print '%d contexts returned.' % len(li)
     #pprint.pprint(li)
     print 'Took %f seconds.' % (time.time() - t)
