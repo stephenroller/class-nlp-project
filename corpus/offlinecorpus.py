63f8f89c5a26a54377f7bf8fe36668d7b5abcbe3
Karl Pichotta <karlpichotta@gmail.com>
2011-04-15 15:43:06 -0500

    braindead O(m*n) implementation

--- corpus/offlinecorpus.py
+++ corpus/offlinecorpus.py
@@ -12,39 +12,47 @@ CONTEXT_SIZE = 10
 DEFAULT_CORPUS = '/u/pichotta/penn-wsj-raw-all.txt'
 PREPRO_FILE_DIR = 'prepro-corpus/'
 
+def canonicalize_corpus_name(cname):
+    return os.path.basename(os.path.normcase(cname))
+
+def get_word_list_filename(cname):
+    return os.path.join(PREPRO_FILE_DIR, cname + '_WORDS')
+
+def get_context_list_dirname(cname):
+    return os.path.join(PREPRO_FILE_DIR, cname + '_CONTEXTS/')
+
+def get_context_list_filename(cname, word):
+    return os.path.join(get_context_list_dirname(cname), word[0], word)
+
+def clean_word(w):
+    return re.sub(r'\W', '', w.lower())
+
 class OfflineCorpus:
 
     def __init__(self, corpusfilename=DEFAULT_CORPUS):
-        self._corpus_name = self._canonicalize_corpus_name(corpusfilename)
+        self._corpus_name = canonicalize_corpus_name(corpusfilename)
         if not self._all_necessary_files_present():
             sys.stderr.write("ERROR! The necessary preprocesed corpus files were not found.\n")
             sys.stderr.write("run offlinecorpuspreprocess.py on the corpus first!\n")
             sys.exit(1)
-        self._corpus_files = self._get_corpus_files(corpusfilename)
-        self._lines = []
-        for f in self._corpus_files:
-            for l in open(f, 'r'):
-                if not self._should_ignore_line(l):
-                    self._lines.append(l)
+        #self._corpus_files = self._get_corpus_files(corpusfilename)
+        #self._lines = []
+        #for f in self._corpus_files:
+        #    for l in open(f, 'r'):
+        #        if not self._should_ignore_line(l):
+        #            self._lines.append(l)
 
     def _all_necessary_files_present(self):
-        return os.path.isfile(self._get_word_list_filename()) and \
-               os.path.isdir(self._get_context_list_dirname())
+        return os.path.isfile(get_word_list_filename(self._corpus_name)) and \
+               os.path.isdir(get_context_list_dirname(self._corpus_name))
 
-    def _canonicalize_corpus_name(self, cname):
-        return os.path.basename(os.path.normcase(cname))
-
-    def _get_word_list_filename(self):
-        return os.path.join(PREPRO_FILE_DIR, self._corpus_name + '_WORDS')
-
-    def _get_context_list_dirname(self):
-        return os.path.join(PREPRO_FILE_DIR, self._corpus_name + '_CONTEXTS/')
-
+    #todo rewrite
     def get_contexts(self, query):
         """returns all contexts in which a query appears in a corpus."""
         return itertools.chain(*[self._get_contexts_in_line(query, l)
                                  for l in self._lines])
 
+    #TODO rewrite
     def get_unique_words(self):
         """returns a set of unique words. Performs some normalization."""
         print 'getting lines...'
@@ -54,38 +62,35 @@ class OfflineCorpus:
         print 'uniquifying...'
         return frozenset().union(*[frozenset(x) for x in cleanlines])
 
-    def _get_corpus_files(self, fname):
-        """takes a filename (optionally a dir), returns list of filenames"""
-        if os.path.isdir(fname):
-            return [x for x in os.walk(fname)]
-        return [fname]
+#    def _get_corpus_files(self, fname):
+#        """takes a filename (optionally a dir), returns list of filenames"""
+#        if os.path.isdir(fname):
+#            return [x for x in os.walk(fname)]
+#        return [fname]
 
     def __iter__(self):
         for l in self._lines:
             yield l
         
-    def _get_contexts_in_line(self, query, line):
-        """might mangle the whitespace a bit but that shouldn't matter"""
-        if self._should_ignore_line(line): return []
-        cleanq = self._clean_word(query)
-        words = line.split()
-        res = []
-        for i in range(len(words)):
-            if self._clean_word(words[i]) == cleanq:
-                res.append(self._get_ind_context(words, i))
-        return res
+#    def _get_contexts_in_line(self, query, line):
+#        """might mangle the whitespace a bit but that shouldn't matter"""
+#        if self._should_ignore_line(line): return []
+#        cleanq = self._clean_word(query)
+#        words = line.split()
+#        res = []
+#        for i in range(len(words)):
+#            if self._clean_word(words[i]) == cleanq:
+#                res.append(self._get_ind_context(words, i))
+#        return res
 
-    def _should_ignore_line(self, line):
-        return line.strip() == '.START'
+#    def _should_ignore_line(self, line):
+#        return line.strip() == '.START'
 
-    def _clean_word(self, w):
-        return re.sub(r'\W', '', w.lower())
 
-    def _get_ind_context(self, words, ind):
-        lb = max(0, ind - CONTEXT_SIZE)
-        ub = min(len(words), ind + CONTEXT_SIZE)
-        #TODO i think there's a performance bottleneck here?
-        return ' '.join(words[lb:ub])
+#    def _get_ind_context(self, words, ind):
+#        lb = max(0, ind - CONTEXT_SIZE)
+#        ub = min(len(words), ind + CONTEXT_SIZE)
+#        return ' '.join(words[lb:ub])
 
 
 if __name__ == '__main__':
--- offlinecorpuspreprocess.py
+++ offlinecorpuspreprocess.py
@@ -12,24 +12,107 @@ make it smart enough to chdir as needed, and didn't want to hardcode the
 absolute path of the corpus preprocessing files in.
 """
 
+from corpus import offlinecorpus
+import itertools
+import os
+import string
 import sys
 
-# this constant is duplicated in corpus.offlinecorpus. Sorry :C
-PREPRO_FILE_DIR = 'prepro-corpus/'
 
 def prepro(corpname):
-    mkdirs(corpname)
+    _mkdirs(corpname)
+    lines = _get_all_lines(_get_corpus_files(corpname))
+    cname = offlinecorpus.canonicalize_corpus_name(corpname)
+    words = _mkwordlist(cname, lines)
+    _mkcontextlist(words, cname, lines)
 
-def mkdirs(corpname):
+def _mkdirs(corpname):
+    cname = offlinecorpus.canonicalize_corpus_name(corpname)
     try:
-        os.mkdir(PREPRO_FILE_DIR)
+        os.mkdir(offlinecorpus.PREPRO_FILE_DIR)
+    except OSError:
+        pass
+    try:
+        os.mkdir(offlinecorpus.get_context_list_dirname(cname))
     except OSError:
         pass
     for letter in string.ascii_lowercase:
         try:
-            os.mkdir(os.join(PREPRO_FILE_DIR, letter))
+            os.mkdir(os.path.join(offlinecorpus.get_context_list_dirname(cname), letter))
         except OSError:
             pass
+
+def _get_corpus_files(fname):
+    """takes a filename (optionally a dir), returns list of filenames"""
+    if os.path.isdir(fname):
+        return [x for x in os.walk(fname)]
+    return [fname]
+
+def _mkwordlist(corpname, lines):
+    """makes word list, returns list of words"""
+    f = open(offlinecorpus.get_word_list_filename(corpname), 'w')
+    words = _get_unique_words(lines)
+    f.writelines([w + '\n' for w in words])
+    f.close()
+    return words
+
+def _mkcontextlist(words, corpname, lines):
+    for w in words:
+        print('getting/writing context for %s' % w)
+        ctxs = _get_contexts(w, lines)
+        _write_contexts(w, corpname, ctxs)
+
+def _write_contexts(word, corpname, contexts):
+    if len(word) == 0 or not word[0].isalpha(): return
+    f = open(offlinecorpus.get_context_list_filename(corpname, word), 'w')
+    f.writelines([x + '\n' for x in contexts])
+    f.close()
+
+def _filter_out_hapax_legomena(words):
+    return [w for w in words if words.count(w) > 1]
+
+def _get_contexts(word, lines):
+    """returns all contexts in which a word appears."""
+    return itertools.chain(*[_get_contexts_in_line(word, l)
+                             for l in lines])
+
+def _get_contexts_in_line(word, line):
+    """might mangle the whitespace a bit but that shouldn't matter"""
+    if _should_ignore_line(line): return []
+    cleanw = offlinecorpus.clean_word(word)
+    words = line.split()
+    res = []
+    for i in range(len(words)):
+        if offlinecorpus.clean_word(words[i]) == cleanw:
+            res.append(_get_ind_context(words, i))
+    return res
+
+def _get_ind_context(words, ind):
+    lb = max(0, ind - offlinecorpus.CONTEXT_SIZE)
+    ub = min(len(words), ind + offlinecorpus.CONTEXT_SIZE)
+    #TODO i think there's a performance bottleneck here?
+    return ' '.join(words[lb:ub])
+
+def _get_unique_words(lines):
+    """returns a set of unique words. Performs some normalization."""
+    # clean and tokenize
+    cleanlines = [[offlinecorpus.clean_word(w) for w in l.split()]
+                  for l in lines]
+    # uniquify
+    return frozenset().union(*[frozenset(x) for x in cleanlines])
+
+def _get_all_lines(files):
+    lines = []
+    for f in files:
+        for l in open(f, 'r'):
+            if not _should_ignore_line(l):
+                lines.append(l)
+    return lines
+    
+def _should_ignore_line(line):
+    return line.strip() == '.START'
+
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
