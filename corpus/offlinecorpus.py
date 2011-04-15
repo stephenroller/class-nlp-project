74ab592bbf3f625407b219e021bf8c5989f96087
Karl Pichotta <karlpichotta@gmail.com>
2011-04-15 04:13:24 -0500

    can get unique words, corpus is iterable

--- corpus/offlinecorpus.py
+++ corpus/offlinecorpus.py
@@ -15,33 +15,33 @@ class OfflineCorpus:
     def __init__(self, corpusfilename=DEFAULT_CORPUS):
         self._corpus_files = self._get_corpus_files(corpusfilename)
 
-    def _get_corpus_files(self, fname):
-        """takes a filename (optionally a dir), returns list of filenames"""
-        if os.path.isdir(fname):
-            return [x for x in os.walk(fname)]
-        return [fname]
-
-    def get_normalized_sent_list(self):
-        return reduce(operator.concat,
-                      [self._get_cleaned_document_list(x)
-                       for x in self._corpus_files])
-        
-    def _get_cleaned_document_list(self, fname):
-        return [self._get_cleaned_tokenized_line(l)
-                for l in open(fname, 'r') 
-                if len(l.strip()) > 0]
-
-    def _get_cleaned_tokenized_line(self, line):
-        """takes a line, returns a list of the cleaned tokens in that line"""
-        return [self._cleanword(w) for w in line.split()
-                if len(self._cleanword(w)) > 0]
-
     def get_contexts(self, query, corpusfilename=DEFAULT_CORPUS):
         """returns all contexts in which a query appears in a corpus."""
         return reduce(operator.concat,
                       [self._get_contexts_in_file(query, x)
                        for x in self._corpus_files])
 
+    def get_unique_words(self):
+        """returns a set of unique words. Performs some normalization."""
+        print 'getting lines...'
+        lines = [l for l in self]
+        print 'cleaning and tokenizing...'
+        cleanlines = [[self._clean_word(w) for w in l.split()] for l in lines]
+        print 'uniquifying...'
+        return frozenset().union(*[frozenset(x) for x in cleanlines])
+
+    def _get_corpus_files(self, fname):
+        """takes a filename (optionally a dir), returns list of filenames"""
+        if os.path.isdir(fname):
+            return [x for x in os.walk(fname)]
+        return [fname]
+
+    def __iter__(self):
+        for f in self._corpus_files:
+            for l in open(f, 'r'):
+                if self._should_ignore_line(l): continue
+                yield l
+        
     def _get_contexts_in_file(self, query, filename):
         return reduce(operator.concat,
                       [self._get_contexts_in_line(query, line)
@@ -50,24 +50,25 @@ class OfflineCorpus:
     def _get_contexts_in_line(self, query, line):
         """might mangle the whitespace a bit but that shouldn't matter"""
         if self._should_ignore_line(line): return []
-        cleanq = self._cleanword(query)
+        cleanq = self._clean_word(query)
         words = line.split()
         res = []
         for i in range(len(words)):
-            if self._cleanword(words[i]) == cleanq:
+            if self._clean_word(words[i]) == cleanq:
                 res.append(self._get_ind_context(words, i))
         return res
 
     def _should_ignore_line(self, line):
         return line.strip() == '.START'
 
-    def _cleanword(self, w):
+    def _clean_word(self, w):
         return re.sub(r'\W', '', w.lower())
 
     def _get_ind_context(self, words, ind):
         lb = max(0, ind - CONTEXT_SIZE)
         ub = min(len(words), ind + CONTEXT_SIZE)
         return ' '.join(words[lb:ub])
+
 
 if __name__ == '__main__':
     q = 'coffee'
