50cb8e231ee1634e9ae0b120e24e51d4a4b46a83
Karl Pichotta <karlpichotta@gmail.com>
2011-04-15 16:21:56 -0500

    using preprocessed stuff now.

--- corpus/offlinecorpus.py
+++ corpus/offlinecorpus.py
@@ -27,6 +27,7 @@ def get_context_list_filename(cname, word):
 def clean_word(w):
     return re.sub(r'\W', '', w.lower())
 
+
 class OfflineCorpus:
 
     def __init__(self, corpusfilename=DEFAULT_CORPUS):
@@ -35,63 +36,25 @@ class OfflineCorpus:
             sys.stderr.write("ERROR! The necessary preprocesed corpus files were not found.\n")
             sys.stderr.write("run offlinecorpuspreprocess.py on the corpus first!\n")
             sys.exit(1)
-        #self._corpus_files = self._get_corpus_files(corpusfilename)
-        #self._lines = []
-        #for f in self._corpus_files:
-        #    for l in open(f, 'r'):
-        #        if not self._should_ignore_line(l):
-        #            self._lines.append(l)
 
     def _all_necessary_files_present(self):
         return os.path.isfile(get_word_list_filename(self._corpus_name)) and \
                os.path.isdir(get_context_list_dirname(self._corpus_name))
 
-    #todo rewrite
     def get_contexts(self, query):
         """returns all contexts in which a query appears in a corpus."""
-        return itertools.chain(*[self._get_contexts_in_line(query, l)
-                                 for l in self._lines])
+        f = open(get_context_list_filename(self._corpus_name,clean_word(query)),
+                 'r')
+        li = [x.strip() for x in f.readlines()]
+        f.close()
+        return li
 
-    #TODO rewrite
     def get_unique_words(self):
         """returns a set of unique words. Performs some normalization."""
-        print 'getting lines...'
-        lines = [l for l in self]
-        print 'cleaning and tokenizing...'
-        cleanlines = [[self._clean_word(w) for w in l.split()] for l in lines]
-        print 'uniquifying...'
-        return frozenset().union(*[frozenset(x) for x in cleanlines])
-
-#    def _get_corpus_files(self, fname):
-#        """takes a filename (optionally a dir), returns list of filenames"""
-#        if os.path.isdir(fname):
-#            return [x for x in os.walk(fname)]
-#        return [fname]
-
-    def __iter__(self):
-        for l in self._lines:
-            yield l
-        
-#    def _get_contexts_in_line(self, query, line):
-#        """might mangle the whitespace a bit but that shouldn't matter"""
-#        if self._should_ignore_line(line): return []
-#        cleanq = self._clean_word(query)
-#        words = line.split()
-#        res = []
-#        for i in range(len(words)):
-#            if self._clean_word(words[i]) == cleanq:
-#                res.append(self._get_ind_context(words, i))
-#        return res
-
-#    def _should_ignore_line(self, line):
-#        return line.strip() == '.START'
-
-
-#    def _get_ind_context(self, words, ind):
-#        lb = max(0, ind - CONTEXT_SIZE)
-#        ub = min(len(words), ind + CONTEXT_SIZE)
-#        return ' '.join(words[lb:ub])
-
+        f = open(get_word_list_filename(self._corpus_name), 'r')
+        li = [x.strip() for x in f.readlines()]
+        f.close()
+        return li
 
 if __name__ == '__main__':
     q = 'coffee'
@@ -103,4 +66,6 @@ if __name__ == '__main__':
     print('Found %d contexts:' % len(cxts))
     for c in cxts:
         print c
+    for w in OfflineCorpus().get_unique_words():
+        print w
 
