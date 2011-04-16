290933e46e50d5771e8b076778a384e631915e31
Stephen Roller <roller@cs.utexas.edu>
2011-04-15 22:18:22 -0500

    Got something kindof working.

--- sembuild.py
+++ sembuild.py
@@ -2,8 +2,9 @@
 
 import sys
 import os
-
+from itertools import islice, count
 from gensim import corpora, models, similarities
+
 from corpus.offlinecorpus import clean_word, OfflineCorpus
 
 DEFAULT_CORPUS = '/u/pichotta/penn-wsj-raw-all.txt'
@@ -15,6 +16,7 @@ class PennCorpus(object):
         self.basename = os.path.basename(filename)
         self.offline = OfflineCorpus()
         self.corpus = None
+        self.docid2word = dict()
         
     def _words_iter(self):
         words_filename = os.path.join(PREPRO_DIR, self.basename + '_WORDS')
@@ -22,6 +24,8 @@ class PennCorpus(object):
             for line in f:
                 line = clean_word(line.strip())
                 if not line:
+                    continue
+                if line in STOP_WORDS:
                     continue
                 yield line
 
@@ -39,30 +43,58 @@ class PennCorpus(object):
             for context in self.corpus:
                 yield context
         else:
-            for word in self._words_iter():
+            i = count(0)
+            for i, word in enumerate(self._words_iter()):
+                self.docid2word[i] = word
                 yield self._get_word_context_vector(word)
 
     def load(self, loaddir=PREPRO_DIR):
         load_path = os.path.join(loaddir, self.basename)
         self.dictionary = corpora.Dictionary.load(load_path + '.dict')
-        self.corpus = corpora.MmCorpus.load(loaddir + '.corpus')
-        self.sim = similarities.SparseMatrixSimilarity.load(loaddir + '.sms')
+        self.corpus = corpora.MmCorpus(load_path + '.corpus')
+        self.sim = similarities.SparseMatrixSimilarity.load(load_path + '.sms')
 
     def save(self, savedir=PREPRO_DIR):
         # create the corpus
         # store copies of the dictionary
         save_path = os.path.join(savedir, self.basename)
-        self.dictionary = corpora.Dictionary(self._words_iter())
+        self.dictionary = corpora.Dictionary([self._words_iter()])
         self.dictionary.save(save_path + '.dict')
         # ... of the corpus
-        corpora.MmCorpus.serialize(save_path + '.corpus', self)
+        corpora.MmCorpus.serialize(save_path + '.corpus', self, id2word=self.dictionary)
         self.corpus = corpora.MmCorpus(save_path + '.corpus')
         self.sim = similarities.SparseMatrixSimilarity(self.corpus)
         # ... and of the similarity matrix
         self.sim.save(save_path + '.sms')
 
+    def similar_to(self, word):
+        def _max_dim(vec):
+            return max(vec, key=lambda x: x[1])
+
+        def _unvec(vec):
+            return dict((self.dictionary[a], b) for a,b in vec)
+
+        if not self.corpus:
+            raise Exception("You must load() or save() first.")
+
+        self.sim.numBest = 10
+        vec = self._get_word_context_vector(word)
+        n = 10
+        retval = []
+        for docid, score in islice(self.sim[vec], n):
+            doc = self.corpus[docid]
+            docword = self.docid2word[docid]
+            retval.append(docword)
+            print docid, docword, score
+        return retval
+
+
+
+
 
 if __name__ == '__main__':
     corpus = PennCorpus(DEFAULT_CORPUS)
     corpus.save()
+    #corpus.load()
+    print corpus.similar_to('coffee')
 
