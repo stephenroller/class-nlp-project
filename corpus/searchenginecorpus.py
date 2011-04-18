2384ee1b1dda17f9bb7f41934d9c784056c173be
Stephen Roller <roller@cs.utexas.edu>
2011-04-18 01:50:00 -0500

    Cache online search queries, and make them storable to disk.

--- corpus/searchenginecorpus.py
+++ corpus/searchenginecorpus.py
@@ -5,9 +5,13 @@ import pprint
 import sys
 import time
 
+from gensim.utils import SaveLoad
+
 APPID = '335CBE48CCCAF4A34652A3DDE7D2CE78FD3390DC'
 
-class SearchEngineCorpus:
+class SearchEngineCorpus(SaveLoad):
+    def __init__(self):
+        self.cache = dict()
 
     def get_contexts(self, query):
         """returns a list of contexts in which a query appears.
@@ -16,6 +20,9 @@ class SearchEngineCorpus:
         times in a returned context, or not at all (if a morphologically
         similar form of the word appears, for example).
         """
+        if query in self.cache:
+            return self.cache[query]
+
         results = WebQuery(APPID,
                            query=query).set_offset(0).set_count(50).execute()
         res = []
@@ -24,6 +31,8 @@ class SearchEngineCorpus:
                 res.append(r.description)
             except AttributeError:
                 pass
+
+        self.cache[query] = res
         return res
 
 if __name__ == '__main__':
