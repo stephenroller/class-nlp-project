cb98ee9deedc9723f059411e14da2d04482c3f25
Stephen Roller <roller@cs.utexas.edu>
2011-04-17 21:29:42 -0500

    Abstract out the word transformer. prolly should go in util module.

--- sembuild.py
+++ sembuild.py
@@ -5,6 +5,7 @@ import os
 import gensim
 
 from nltk.corpus import WordNetCorpusReader
+from nltk.stem.porter import PorterStemmer
 
 from corpus.offlinecorpus import clean_word, OfflineCorpus
 
@@ -12,31 +13,57 @@ DEFAULT_CORPUS = '/u/pichotta/penn-wsj-raw-all.txt'
 PREPRO_DIR = 'prepro-corpus/'
 STOP_WORDS = ['the', 'an', 'a', '.start', 'to', 'of', 'and']
 
+
+class WordTransformer(object):
+    def __init__(self):
+        self.stemmer = PorterStemmer()
+
+    def transform(self, word):
+        # stems, lowercases and checks if stopword
+        # returns None if the word should be ignored.
+        w = word.lower().strip()
+        w = clean_word(w)
+        #w = self.stemmer.stem(w)
+        if w in STOP_WORDS:
+            return None
+        return w
+
+
 class StorableDictionary(dict, gensim.utils.SaveLoad):
     pass
 
+
 class PennCorpus(object):
     def __init__(self, offline):
+        self.transformer = WordTransformer()
         self.offline = offline
         self.docid2word = StorableDictionary()
         self.word2docid = StorableDictionary()
         self.dictionary = gensim.corpora.dictionary.Dictionary([self._words_iter()])
-        
+
     def _words_iter(self):
         for word in self.offline.get_unique_words():
-            word = clean_word(word)
-            if not word or word in STOP_WORDS:
-                continue
-            yield word
+            word = self.transformer.transform(word)
+            if word:
+               yield word
+
+    def _context_to_vector(self, word, context, ignore_word=False):
+        context = context.lower().strip().split(' ')
+        context = [clean_word(w) for w in context]
+        if ignore_word:
+            # optionally, we can exclude the word itself from the context
+            # vector
+            context = [w for w in context if w != word]
+        context = [self.transformer.transform(w) for w in context]
+        # filter out words we're supposed to ignore
+        context = [w for w in context if w]
+        return context
+
 
     def _get_word_context_vector(self, word):
         all_contexts = []
         for context in self.offline.get_contexts(word):
-            context = context.lower().strip().split(' ')
-            context = [clean_word(w) for w in context]
-            context = [w for w in context if w not in STOP_WORDS]
-            #context = [w for w in context if w != word]
-            all_contexts += context
+            all_contexts += self._context_to_vector(word, context)
         return self.dictionary.doc2bow(all_contexts)
 
     def __iter__(self):
@@ -94,9 +121,12 @@ class CorpusSimilarityFinder(object):
         if not self.corpus:
             raise Exception("You must load() or save() first.")
 
+        wt = WordTransformer()
+        word = wt.transform(word)
         n = 10
         self.sim.numBest = n
         vec = self.corpus[self.word2docid[word]]
+        vec = self.tfidf[vec]
 
         retval = []
         for docid, score in self.sim[vec]:
