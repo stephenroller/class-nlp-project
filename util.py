6223b4b80e85fef75e5b7d79a2cc3b8223225eac
Stephen Roller <roller@cs.utexas.edu>
2011-04-19 18:41:47 -0500

    Large stop words list.

--- util.py
+++ util.py
@@ -4,6 +4,31 @@ import re
 import gensim
 
 STOP_WORDS = ['the', 'an', 'a', 'to', 'of', 'and', 'in']
+LARGE_STOP_WORDS = [
+    'a', 'about', 'above', 'after', 'again', 'against', 'all', 'am',
+    'an', 'and', 'any', 'are', 'arent', 'as', 'at', 'be', 'because',
+    'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by',
+    'cannot', 'cant', 'could', 'couldnt', 'did', 'didnt', 'do', 'does',
+    'doesnt', 'doing', 'dont', 'down', 'during', 'each', 'few', 'for',
+    'from', 'further', 'had', 'hadnt', 'has', 'hasnt', 'have', 'havent',
+    'having', 'he', 'hed', 'hell', 'her', 'here', 'heres', 'hers',
+    'herself', 'hes', 'him', 'himself', 'his', 'how', 'hows', 'i', 'id',
+    'if', 'ill', 'im', 'in', 'into', 'is', 'isnt', 'it', 'its',
+    'itself', 'ive', 'lets', 'me', 'more', 'most', 'mustnt', 'my',
+    'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only',
+    'or', 'other', 'ought', 'our', 'ours', 'ourselves', 'out', 'over',
+    'own', 'same', 'shant', 'she', 'shed', 'shell', 'shes', 'should',
+    'shouldnt', 'so', 'some', 'such', 'than', 'that', 'thats', 'the',
+    'their', 'theirs', 'them', 'themselves', 'then', 'there', 'theres',
+    'these', 'they', 'theyd', 'theyll', 'theyre', 'theyve', 'this',
+    'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very',
+    'was', 'wasnt', 'we', 'wed', 'well', 'were', 'werent', 'weve',
+    'what', 'whats', 'when', 'whens', 'where', 'wheres', 'which',
+    'while', 'who', 'whom', 'whos', 'why', 'whys', 'with', 'wont',
+    'would', 'wouldnt', 'you', 'youd', 'youll', 'your', 'youre',
+    'yours', 'yourself', 'yourselves', 'youve'
+]
+STOP_WORDS = LARGE_STOP_WORDS
 
 class WordTransformer(object):
     def __init__(self, stemming=False):
