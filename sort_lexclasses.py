39bb511ebe7f0907f5613d2418bd6f9e3359c143
Stephen Roller <roller@cs.utexas.edu>
2011-04-19 18:42:25 -0500

    Sort_lexclasses update to use indexed_corpus.

--- sort_lexclasses.py
+++ sort_lexclasses.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
-from corpus.offlinecorpus import OfflineCorpus
+import sys
+
+from corpus.indexedcorpus import IndexedCorpus
 from nltk.corpus import WordNetCorpusReader
 
-offline = OfflineCorpus()
-wn = WordNetCorpusReader('wordnet/1.6/')
+def yield_single_sense_nouns_in_corpus(corpus):
+    wn = WordNetCorpusReader('wordnet/1.6/')
+    for word in corpus.get_unique_words():
+        synsets = list(wn.synsets(word))
+        if len(synsets) == 1 and 'noun' in synsets[0].lexname:
+            yield word
 
-for word in offline.get_unique_words():
-    synsets = list(wn.synsets(word))
-    if len(synsets) == 1 and 'noun' in synsets[0].lexname:
+if __name__ == '__main__':
+    index_file = sys.argv[1]
+    indexed_corpus = IndexedCorpus(index_file)
+    for word in yield_single_sense_nouns_in_corpus(indexed_corpus):
         print word
 
 
