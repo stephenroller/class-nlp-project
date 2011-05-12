2fe3d501bbd0804e6d6d535070687284c5eaa969
Karl Pichotta <karlpichotta@gmail.com>
2011-05-12 08:51:48 -0500

    a few words on analysis; a new image

--- writeup/graphs/generate-n-graph.R
+++ writeup/graphs/generate-n-graph.R
@@ -7,14 +7,20 @@ acc <- c(0.373984,
          0.449612,
          0.412698,
          0.437500,
-         0.437500)
+         0.437500,
+         0.449612,
+         0.484848,
+         0.484848,
+         0.473282,
+         0.461538,
+         0.461538)
 
 
 # Graph cars using a y axis that ranges from 0 to 12
 plot(acc, type="o", col="blue", ylim=c(0.3,0.5), axes=FALSE, ann=FALSE)
 
 
-axis(1, at=1:7, lab=2*2:8)
+axis(1, at=1:13, lab=2*2:14)
 axis(2, at=0.3+(0.04*0:5))
 
 title(xlab="n")
